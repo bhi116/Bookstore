26729ee3c92bf9b5896645efd08f5b550a40a982
bhi116 <bhi116@myy.haaga-helia.fi>
2025-02-06 23:33:50 +0200

    Booklist.html + endpoint

--- bookstore/src/main/java/nl/bookstore/web/BookController.java
+++ bookstore/src/main/java/nl/bookstore/web/BookController.java
@@ -1,15 +1,21 @@
 package nl.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import nl.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 
-    @GetMapping("/index")
-    @ResponseBody
-    public String index() {
-        return "Welcome to the Bookstore!";
+    @Autowired
+    private BookRepository repository;
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }
