418f5c9730e5bf2165e984987f0dfcf006fd855e
bhi116 <bhi116@myy.haaga-helia.fi>
2025-02-08 13:21:47 +0200

    Edit book

--- bookstore/src/main/java/nl/bookstore/web/BookController.java
+++ bookstore/src/main/java/nl/bookstore/web/BookController.java
@@ -1,9 +1,13 @@
 package nl.bookstore.web;
 
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import nl.bookstore.domain.Book;
@@ -38,4 +42,31 @@ public class BookController {
         repository.deleteById(bookId);
         return "redirect:/booklist";
     }
+
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        Optional<Book> book = repository.findById(bookId);
+        if (book.isPresent()) {
+            model.addAttribute("book", book.get());
+            return "editbook";
+        }
+        return "redirect:/booklist";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateBook(@PathVariable("id") Long bookId, @ModelAttribute Book updatedBook) {
+        Optional<Book> bookData = repository.findById(bookId);
+        if (bookData.isPresent()) {
+            Book book = bookData.get();
+            book.setTitle(updatedBook.getTitle());
+            book.setAuthor(updatedBook.getAuthor());
+            book.setPublicationYear(updatedBook.getPublicationYear());
+            book.setIsbn(updatedBook.getIsbn());
+            book.setPrice(updatedBook.getPrice());
+
+            repository.save(book);
+        }
+        return "redirect:/booklist";
+    }
+
 }
