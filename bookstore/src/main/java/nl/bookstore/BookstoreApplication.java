730da9e55d0d7808875e0d44a4c2ff2526790c66
bhi116 <bhi116@myy.haaga-helia.fi>
2025-02-18 15:44:08 +0200

    Kategorioiden lisäys

--- bookstore/src/main/java/nl/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/nl/bookstore/BookstoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 
 import nl.bookstore.domain.Book;
 import nl.bookstore.domain.BookRepository;
+import nl.bookstore.domain.Category;
+import nl.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -16,8 +18,9 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(BookRepository repository) {
+	public CommandLineRunner demo(BookRepository bookRepository, CategoryRepository categoryRepository) {
 		return (args) -> {	
+			// lisätään kirjoja
 			Book b1 = new Book("Harry Potter ja liekehtivä pikari", "J.K. Rowling", 2001, 
 			"9789513187057", 30.95);
 			Book b2 = new Book("Harry Potter ja salaisuuksien kammio", "J.K. Rowling", 1999, 
@@ -25,9 +28,21 @@ public class BookstoreApplication {
 			Book b3 = new Book("Harry Potter ja Feeniksin kilta", "J.K. Rowling", 2003, 
 			"9789520401955", 28.95);
 
-			repository.save(b1);
-			repository.save(b2);
-			repository.save(b3);
+			// tallennetaan kirjat tietokantaan
+			bookRepository.save(b1);
+			bookRepository.save(b2);
+			bookRepository.save(b3);
+
+			// lisätään kategorioita
+			Category adventure = new Category("Adventure");
+			Category comic = new Category("Comic");
+			Category fantasy = new Category("Fantasy");
+
+			// tallennetaan kategoriat tietokantaan
+			categoryRepository.save(adventure);
+			categoryRepository.save(comic);
+			categoryRepository.save(fantasy);
+
 		};
 	}
 
---
+++ bookstore/src/main/java/nl/bookstore/domain/CategoryRepository.java
@@ -0,0 +1,7 @@
+package nl.bookstore.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long>{
+
+}
